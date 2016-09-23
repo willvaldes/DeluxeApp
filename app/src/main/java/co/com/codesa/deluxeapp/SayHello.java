9a51babd7a8908c5b3860700985b78d64a14e98f
William David Valdes Martinez <will.valdes@gmail.com>
2016-09-23 13:46:20 -0500

    dos

---
+++ app/src/main/java/co/com/codesa/deluxeapp/SayHello.java
@@ -0,0 +1,13 @@
+package co.com.codesa.deluxeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SayHello extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_say_hello);
+    }
+}
